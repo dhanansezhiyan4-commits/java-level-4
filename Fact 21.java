56a30bcfac6906c7ceebc3a20a0a216c1b32a1ae
dhana <dhanansezhiyan4@gmail.com>
2026-03-23 12:13:06 +0530

    Correct factorial program implementation
    Fixed syntax errors and improved output messages in the factorial program.

--- Fact 21.java
+++ Fact 21.java
@@ -1,25 +1,26 @@
 import java.io.*;
- 
-class Fact 
-{
- public static void main(String args[])
-{
- InputStreamReader isr=new InputStreamReader(System.in);
- BufferedReader br=new BufferedReader (isr);
 
-        System.out.println("Factoical program");
-	System.out.println("_________________");
-	System.out.println("enter the number");
-	String s1=br.readLine();
-	int n=Integer.parseInt(s1);
-	System.out.println("report");
-	
-	int Fact : 1
-	for(int i=1;i<=n;i++);
-	{
-	  System.out.println("*" +I);
-	   sum=sum * i;
-	}
-	 System.out.println("=" +sum);
-	}
-     }
+class Fact {
+    public static void main(String args[]) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        System.out.println("Factorial program");
+        System.out.println("_________________");
+        System.out.println("Enter the number:");
+
+        String s1 = br.readLine();
+        int n = Integer.parseInt(s1);
+
+        System.out.println("Report:");
+
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("* " + i);
+            fact = fact * i;
+        }
+
+        System.out.println("= " + fact);
+    }
+}
